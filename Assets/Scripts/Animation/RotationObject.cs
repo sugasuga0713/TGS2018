9e63effa35246ea2d585d5d54cb48597cb3ef2fa
Ysugashi <sugashi0713@google.com>
2018-04-16 16:45:09 +0900

    no message

---
+++ Assets/Scripts/Animation/RotationObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObject : ManagedUpdateBehaviour {
+
+	[SerializeField] private float rotateSpeed = 1.0f;
+	private Transform myTransform;
+	private Vector3 rotateDir;
+
+	public override void FixedUpdateMe()
+	{
+		myTransform.Rotate(rotateDir);
+	}
+
+	protected override void Initialize()
+	{
+		myTransform = GetComponent<Transform>();
+		rotateDir = new Vector3(0.0f,0.0f,rotateSpeed);
+	}
+}
